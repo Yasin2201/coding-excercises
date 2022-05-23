140083e77f8b1e3e4abd23555d7b993ce55443b6
Yasin Amin <yasin.amin@hotmail.co.uk>
2022-05-23 20:47:59 +0100

    add coinChange functionality

--- coin_change/coinChange.js
+++ coin_change/coinChange.js
@@ -1,5 +1,14 @@
-function coinChange() {
-    return ''
+function coinChange(coinsArray, n) {
+    let requiredCoins = {};
+
+    for (i = 0; i < coinsArray.length; i++) {
+        if (Math.floor(n / coinsArray[i])) {
+            requiredCoins[`${coinsArray[i]}p`] = Math.floor(n / coinsArray[i]);
+            n -= coinsArray[i] * Math.floor(n / coinsArray[i])
+        }
+    }
+
+    return requiredCoins
 }
 
 module.exports = coinChange;
